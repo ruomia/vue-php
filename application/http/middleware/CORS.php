f1f28ec1813d497c5804b99fa5fe069d896b5d48
humie <hyp@czxy.com>
2019-05-08 21:13:33 +0800

    aaaa

--- application/http/middleware/CORS.php
+++ application/http/middleware/CORS.php
@@ -1,22 +1,27 @@
 <?php
 namespace app\http\middleware;
 
+use think\Response;
 class CORS
 {
     public function handle($request, \Closure $next)
     {
-        $response = $next($request);
-        $origin = $request->server('HTTP_ORIGIN') ?: '';
-        $allow_origin = [
-            'http://zz.cnguu.cn',
-        ];
+        // $origin = $request->server('HTTP_ORIGIN') ?: '';
+        // $allow_origin = [
+        //     'http://zz.cnguu.cn',
+        // ];
         // if (in_array($origin, $allow_origin)) {
-            $response->header('Access-Control-Allow-Origin', '*');
-            $response->header('Access-Control-Allow-Headers', 'Authorization, Content-Type, Accept, Origin, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-Requested-With, X-Id, X-Token');
-            $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
-            $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-            $response->header('Access-Control-Allow-Credentials', 'true');
-        // }
-        return $response;
+
+        // // }
+        // return $next($request);
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Headers: Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-Requested-With');
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE');
+        header('Access-Control-Max-Age: 1728000');
+        if (strtoupper($request->method()) == "OPTIONS") {
+            return Response::create();
+        }
+
+        return $next($request);
     }
 }
